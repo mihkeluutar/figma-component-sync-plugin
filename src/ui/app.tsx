11752782c8b312866325bec943c83081f3d24fc0
Mihkel Uutar <miuuta@taltech.ee>
2025-02-10 23:07:47 +0200

    Removes networker stuff

--- src/ui/app.tsx
+++ src/ui/app.tsx
@@ -1,22 +1,6 @@
-import { PLUGIN } from "@common/networkSides";
-import { UI_CHANNEL } from "@ui/app.network";
-import { Button } from "@ui/components/Button";
-import { Networker, NetworkError } from "monorepo-networker";
-import { useEffect, useState } from "react";
-import { getAllComponentSets } from "./utils/figma-utils";
-
 import "./styles.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [pingCount, setPingCount] = useState(0);
-
-  useEffect(() => {
-    UI_CHANNEL.subscribe("ping", () => {
-      setPingCount((cnt) => cnt + 1);
-    });
-  }, []);
-
   return (
     <div className="homepage">
       <h1>Figma + Vite + React</h1>
@@ -26,46 +10,6 @@ function App() {
       </select>
 
       <div className="card">
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
-        <Button
-          onClick={async () => {
-            const response = await UI_CHANNEL.request(PLUGIN, "ping", []);
-            console.log("Response:", response);
-          }}
-          style={{ marginInlineStart: 10 }}
-        >
-          ping the other side
-        </Button>
-        <Button
-          onClick={() => {
-            console.log("Create a rectangle, please!");
-            UI_CHANNEL.emit(PLUGIN, "createRect", [100, 100]);
-          }}
-          style={{ marginInlineStart: 10 }}
-        >
-          create square
-        </Button>
-        <Button
-          onClick={async () => {
-            try {
-              const result = await UI_CHANNEL.request(
-                PLUGIN,
-                "exportSelection",
-                []
-              );
-              console.log("Export: ", { result });
-            } catch (err) {
-              if (err instanceof NetworkError) {
-                console.log("Couldn't export..", { message: err.message });
-              }
-            }
-          }}
-          style={{ marginInlineStart: 10 }}
-        >
-          export selection
-        </Button>
         <p>
           Edit <code>src/app.tsx</code> and save to test HMR
         </p>
